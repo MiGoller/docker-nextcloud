f05b36e2de4e01616987ae334a54a1dc2196d9c6
Skunkworx <1272642+MiGoller@users.noreply.github.com>
2018-09-16 19:29:45 +0200

    Further test on Travis CI

---
+++ scripts/travisBuildImage.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+sed -ev
+
+#   Login to Docker Hub
+echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
+
+#   Build image from Dockerfile in ./apache/$2
+docker build -f ./apache/$2/Dockerfile -t $1-$TRAVIS_BRANCH-$2 ./apache/$2
+
+# Tag and push image for each additional tag
+for tag in {"${NEXTCLOUD_VERSION}-apache-$2","${version[0]}.${version[1]}-apache-$2","${version[0]}-apache-$2"}; do  
+    docker tag $1-$TRAVIS_BRANCH-$2 $DOCKER_USERNAME/$1:$TRAVIS_BRANCH-${tag}
+    docker push $DOCKER_USERNAME/$1:$TRAVIS_BRANCH-${tag}
+done
