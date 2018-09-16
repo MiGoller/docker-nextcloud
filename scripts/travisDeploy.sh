baf2a25d3a2220ad838cd4222239cb2864442e0c
Skunkworx <1272642+MiGoller@users.noreply.github.com>
2018-09-16 11:39:15 +0200

    Multi-image deploy test Travis CI

--- scripts/travisDeploy.sh
+++ scripts/travisDeploy.sh
@@ -10,8 +10,14 @@ export $(docker inspect --format='{{range .Config.Env}}{{println .}}{{end}}' nex
 version=( ${NEXTCLOUD_VERSION//./ } )
 # TAG naming: version="${version[0]}.${version[1]}.${version[2]}"
 
-# Tag and push image for each additional tag
+# Tag and push image for each additional tag: Base-image
 for tag in {"${NEXTCLOUD_VERSION}-apache","${version[0]}.${version[1]}-apache","${version[0]}-apache","${version[0]}-stable-apache","${version[0]}-production-apache","${version[0]}-stable","${version[0]}-production","${version[0]}-latest"}; do  
-    docker tag docker-nextcloud-stage $DOCKER_USERNAME/travistest:${tag}
-    docker push $DOCKER_USERNAME/travistest:${tag}
+    docker tag docker-nextcloud-$TRAVIS_BRANCH-base $DOCKER_USERNAME/travistest:$TRAVIS_BRANCH-${tag}
+    docker push $DOCKER_USERNAME/travistest:$TRAVIS_BRANCH-${tag}
+done
+
+# Tag and push image for each additional tag: Preview-image
+for tag in {"${NEXTCLOUD_VERSION}-apache-preview","${version[0]}.${version[1]}-apache-preview","${version[0]}-apache-preview"}; do  
+    docker tag docker-nextcloud-$TRAVIS_BRANCH-preview $DOCKER_USERNAME/travistest:$TRAVIS_BRANCH-${tag}
+    docker push $DOCKER_USERNAME/travistest:$TRAVIS_BRANCH-${tag}
 done
