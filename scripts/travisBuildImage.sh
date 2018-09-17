2c2190c84a503662bc34d053e5ab7f4a7924b686
Skunkworx <1272642+MiGoller@users.noreply.github.com>
2018-09-17 16:00:43 +0200

    Another try ...

--- scripts/travisBuildImage.sh
+++ scripts/travisBuildImage.sh
@@ -8,8 +8,21 @@ echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 #   Build image from Dockerfile in ./apache/$2
 docker build -f ./apache/$2/Dockerfile -t $1-$TRAVIS_BRANCH-$2 ./apache/$2
 
+#   Get Nextcloud version information from image
+docker images
+
+export $(docker inspect --format='{{range .Config.Env}}{{println .}}{{end}}' $1-$TRAVIS_BRANCH-$2 | grep NEXTCLOUD_VERSION)
+
+#   Define tag suffix
+case "$2" in
+ base) export TAG_SUFFIX="-apache" ;;
+    *) export TAG_SUFFIX="-apache-$2" ;;
+esac
+export TAG_SUFFIX="-apache-$2"
+
 # Tag and push image for each additional tag
-for tag in {"${NEXTCLOUD_VERSION}-apache-$2","${version[0]}.${version[1]}-apache-$2","${version[0]}-apache-$2"}; do  
-    docker tag $1-$TRAVIS_BRANCH-$2 $DOCKER_USERNAME/$1:$TRAVIS_BRANCH-${tag}
-    docker push $DOCKER_USERNAME/$1:$TRAVIS_BRANCH-${tag}
+for tag in {"${NEXTCLOUD_VERSION}","${version[0]}.${version[1]}","${version[0]}"}; do  
+    echo "--> Tagging docker image: $DOCKER_USERNAME/$1:${tag}$TAG_SUFFIX"
+    docker tag $1-$TRAVIS_BRANCH-$2 $DOCKER_USERNAME/$1:${tag}$TAG_SUFFIX
+    docker push $DOCKER_USERNAME/$1:${tag}$TAG_SUFFIX
 done
